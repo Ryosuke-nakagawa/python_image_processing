619d2cd6bc47f7300e7a287e290c982b624d6c5f
Ryosuke <tffcd0514@gmail.com>
2022-09-11 22:29:21 +0900

    add: 画像処理

--- milkdrop.py
+++ milkdrop.py
@@ -1,13 +1,29 @@
 import cv2
+import numpy as np
 
-img = cv2.imread("./milkdrop.bmp", 0)
+img = cv2.imread("./milkdrop.bmp")
+
+cv2.imshow('preview', img)
+cv2.waitKey()
+
+img_grayscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 threshold = 135
 
-#大津の二値化に使用, 二値化された画像 = cv2.threshold(読み込んだ画像①, しきい値②, 変換後の値③, 二値化手法④)
-ret, img_thresh = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+_, img_thresh = cv2.threshold(img_grayscale, threshold, 255, cv2.THRESH_BINARY)
 
+height, width = img_thresh.shape[:2]
 
-cv2.imshow("img_th", img_thresh)
+img_thresh_trim = img_thresh[100 : height, 100 : width]
+
+contours, hierarchy = cv2.findContours(img_thresh_trim, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+contour = list(filter(lambda x: cv2.contourArea(x) >= 1000, contours))
+
+mask = np.zeros(img.shape, dtype = np.uint8)
+drawed_mask = cv2.drawContours(mask, contour, -1, (255, 255, 255), -1, offset=(100,100))
+
+output = cv2.bitwise_and(img, drawed_mask )
+
+cv2.imshow("output", output)
 cv2.waitKey()
 cv2.destroyAllWindows()
